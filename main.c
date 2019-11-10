06f79c850f927ddbc3c3ff7f2c9b8cbf4f68e3a6
Tecnelm <cleman91@gmail.com>
2019-11-10 23:41:20 +0100

    correction to MakeList and Makefile
    creation of a folder src to put the .c and .h

--- main.c
+++ main.c
@@ -1,24 +1,26 @@
 #include <stdio.h>
 #include "src/fat.h"
 
-int main() {
-    char nom[] = "first";
-    char nom2[] = "frfst";
-    unsigned short auteur = 7;
-    char data[] = "je moupel pedro carahol!";
-    unsigned int taille = sizeof(data);
 
-    //char* pnom;
-    //pnom = &nom;
+int main () {
 
-    struct objet *objTemp;
+	char nom[] = "first";
+	char nom2[] = "frfst";
+	unsigned short auteur = 7;
+	char data[] = "je moupel pedro carahol!";
+	unsigned int taille = sizeof(data);
 
-    initialise_fat();
-    creer_objet( nom,   auteur,  taille,  data);
+	//char* pnom;
+	//pnom = &nom;
 
-    objTemp =  rechercher_objet('first');
-    printf("%d",objTemp->taille);
+	struct objet *objTemp;
+
+	initialise_fat();
+	creer_objet(nom, auteur, taille, data);
+
+	objTemp = rechercher_objet('first');
+	printf("%d", objTemp->taille);
 
 
-    return 0;
+	return 0;
 }
